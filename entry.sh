11abc1a6be0448326ef9ad97aaf4b9a758f606bd
Alexandre <13389415+akeiflin70@users.noreply.github.com>
2023-01-26 20:55:45 +0100

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-CONFIG=/config/config.json
-log=/bing-rewards/BingRewards/logs/error.log
+#CONFIG=/config/config.json
+#log=/bing-rewards/BingRewards/logs/error.log
 
 set -x
 
-echo "Setting Up pip and installing requirements"
+#echo "Setting Up pip and installing requirements"
 #pip install --upgrade pip
-pip install --no-warn-script-location -r /bing-rewards/BingRewards/requirements.txt
-echo "Checking for imported logs"
+#pip install --no-warn-script-location -r /bing-rewards/BingRewards/requirements.txt
+#echo "Checking for imported logs"
 
-echo "Making logs To tail"
-mkdir /bing-rewards/BingRewards/logs
-touch /bing-rewards/BingRewards/logs/cronBing.log
-touch /bing-rewards/BingRewards/logs/error.log
-cd /bing-rewards/BingRewards
+#echo "Making logs To tail"
+#mkdir /bing-rewards/BingRewards/logs
+#touch /bing-rewards/BingRewards/logs/cronBing.log
+#touch /bing-rewards/BingRewards/logs/error.log
+#cd /bing-rewards/BingRewards
 #tail -f /bing-rewards/BingRewards/logs/cronBing.log
